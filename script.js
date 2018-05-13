570ede185e30a0d50790a3014d08ef596d49ebce
sampsonmao <sampson.mao@gmail.com>
2018-05-12 17:11:51 -0700

    Black and white style added, centered drawing pad and changed to static size

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const flexContainer = document.querySelector('.flexContainer')
 
-let gridSize = prompt('Enter grid size')
+let gridSize = 50
 let numBoxes = 0
 while (numBoxes < gridSize**2) {
     let box = document.createElement('div')
     box.classList.add('box')
-    box.style.height = 100/gridSize + '%'
-    box.style.width = 100/gridSize + '%'
+    box.style.height = 500/gridSize + 'px'
+    box.style.width = 500/gridSize + 'px'
     flexContainer.appendChild(box)
     box.addEventListener('mouseenter', changeColor)
     numBoxes++ 
@@ -20,6 +20,6 @@ function changeColor(e) {
 function erase() {
     let boxes = document.body.getElementsByClassName('box')
     for (i = 0; i<boxes.length; i++) {
-        boxes[i].style.background = "dodgerblue"
+        boxes[i].style.background = "white"
        }
     }
